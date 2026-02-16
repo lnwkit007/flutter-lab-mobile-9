418e002c0467b9b4162fdfa24fa09c4141b4f67f
lnwkit007 <164681756+lnwkit007@users.noreply.github.com>
2026-02-16 21:08:00 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:lab9/screen/displayscreen.dart';
+import 'package:lab9/screen/formscreen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2, 
+      child: Scaffold(
+        body: TabBarView(children: [FormScreen(), DisplayScreen()]),
+        bottomNavigationBar: TabBar(
+          tabs: [
+            Tab(text: 'บันทึกอุณหภูมิ'),
+            Tab(text: 'รายชื่อนักเรียน'),
+          ],
+        ),
+        backgroundColor: Colors.blue,
+      )
+    );
+  }
+}
